35edca9f736edf91911cb31a47f81f989f433347
U-HAULMONT\fedorov <fedorov@fedorov.haulmont.com>
2015-02-13 17:54:39 +0400

    emacs package auto-install

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -10,6 +10,38 @@
 ;;; loads packages and activates them
 (package-initialize)
 
+;;; packages to install
+(setq package-list '(color-theme-sanityinc-tomorrow
+		     evil
+		     goto-chg
+		     monokai-theme
+		     undo-tree))
+
+(defun ensure-package-installed (&rest packages)
+  "Assure every package is installed, ask for installation if it’s not.
+
+Return a list of installed packages or nil for every skipped package."
+  (mapcar
+   (lambda (package)
+     ;; (package-installed-p 'evil)
+     (if (package-installed-p package)
+         nil
+       (if (y-or-n-p (format "Package %s is missing. Install it? " package))
+           (package-install package)
+         package)))
+   packages))
+
+;; make sure to have downloaded archive description.
+;; Or use package-archive-contents as suggested by Nicolas Dudebout
+(or (file-exists-p package-user-dir)
+    (package-refresh-contents))
+
+(dolist (package package-list)
+  (ensure-package-installed package)) ;  --> (nil nil) if iedit and magit are already installed
+
+;;; loads packages and activates them
+(package-initialize)
+
 ;;; evil mode by default
 (require 'evil)
 (evil-mode t)
