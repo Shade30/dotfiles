5387e57c9ebd4d6c15702d771f90e617a0a2a7c0
Sergey Fedorov <fedorov@haulmont.com>
2016-10-19 16:08:39 +0400

    removed customized config

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -5,19 +5,19 @@
 (add-to-list 'package-archives '("marmalade" . "https://marmalade-repo.org/packages/"))
 (add-to-list 'package-archives '("org" . "http://orgmode.org/elpa/"))
 (add-to-list 'package-archives
-                         '("melpa" . "http://melpa.milkbox.net/packages/") t)
+			 '("melpa" . "http://melpa.milkbox.net/packages/") t)
 
 ;;; loads packages and activates them
 (package-initialize)
 
 ;;; packages to install
 (setq package-list '(color-theme-sanityinc-tomorrow
-                     evil
-                     goto-chg
-                     monokai-theme
-                     undo-tree
+		     evil
+		     goto-chg
+		     monokai-theme
+		     undo-tree
                      smooth-scrolling
-                     ace-jump-mode
+		     ace-jump-mode
                      icicles
                      cider
                      clojure-mode
@@ -304,63 +304,6 @@ Return a list of installed packages or nil for every skipped package."
 
 ;; load in customizations
 (if (file-exists-p "~/init_custom.el") (load-library "~/init_custom.el"))
-
-;;; added automatically
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(ansi-color-faces-vector
-   [default bold shadow italic underline bold bold-italic bold])
- '(ansi-color-names-vector
-   (vector "#ffffff" "#ff9da4" "#d1f1a9" "#ffeead" "#bbdaff" "#ebbbff" "#99ffff" "#002451"))
- '(custom-enabled-themes (quote (sanityinc-tomorrow-night)))
- '(custom-safe-themes
-   (quote
-    ("06f0b439b62164c6f8f84fdda32b62fb50b6d00e8b01c2208e55543a6337433a" "82d2cac368ccdec2fcc7573f24c3f79654b78bf133096f9b40c20d97ec1d8016" "628278136f88aa1a151bb2d6c8a86bf2b7631fbea5f0f76cba2a0079cd910f7d" "4e262566c3d57706c70e403d440146a5440de056dfaeb3062f004da1711d83fc" default)))
- '(fci-rule-color "#00346e")
- '(inhibit-startup-screen t)
- '(org-agenda-files (quote ("~/org/main.org")))
- '(package-selected-packages
-   (quote
-    (smooth-scrolling rainbow-delimiters projectile nyan-mode monokai-theme icicles htmlize groovy-mode flx-ido evil color-theme-sanityinc-tomorrow cider-eval-sexp-fu cider auto-complete ace-jump-mode)))
- '(sql-connection-alist
-   (quote
-    (("pg_beirut_trunk"
-      (sql-product
-       (quote postgres))
-      (sql-user "root")
-      (sql-database "taxi_beirut")
-      (sql-server "localhost")))))
- '(vc-annotate-background nil)
- '(vc-annotate-color-map
-   (quote
-    ((20 . "#ff9da4")
-     (40 . "#ffc58f")
-     (60 . "#ffeead")
-     (80 . "#d1f1a9")
-     (100 . "#99ffff")
-     (120 . "#bbdaff")
-     (140 . "#ebbbff")
-     (160 . "#ff9da4")
-     (180 . "#ffc58f")
-     (200 . "#ffeead")
-     (220 . "#d1f1a9")
-     (240 . "#99ffff")
-     (260 . "#bbdaff")
-     (280 . "#ebbbff")
-     (300 . "#ff9da4")
-     (320 . "#ffc58f")
-     (340 . "#ffeead")
-     (360 . "#d1f1a9"))))
- '(vc-annotate-very-old-color nil))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
 
 ;; highlight hex colors
 (defun xah-syntax-color-hex ()
