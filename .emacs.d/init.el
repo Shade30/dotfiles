e38097271978780b01cd80e1b8628e455465e3c5
Sergey Fedorov <sergey.fedorov@haulmont.com>
2026-03-24 15:27:54 +0400

    Emacs - removed trello integration

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -238,20 +238,6 @@
             (org-agenda-remove-tags t))
            ("~/org/theagenda.html"))))
 
-;;; org-trello
-(use-package org-trello
-  :ensure t
-  ;; org-trello major mode for all .trello files
-  :mode ("\\.trello$" . org-mode)
-
-  :init
-  ;; add a hook function to check if this is trello file, then activate the org-trello minor mode.
-  (add-hook 'org-mode-hook
-            (lambda ()
-              (let ((filename (buffer-file-name (current-buffer))))
-                (when (and filename (string= "trello" (file-name-extension filename)))
-                  (org-trello-mode))))))
-
 ;;; secretaria
 (use-package secretaria
   :ensure t
